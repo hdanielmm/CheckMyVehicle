c2b98d716dc10c68a72dd2e5fb33fa35ae9c0610
Daniel <hdanielmm@gmail.com>
2020-02-02 11:23:27 -0500

    Add data for vehiculo, parteVehiculo and empleado

--- src/db/query.sql
+++ src/db/query.sql
@@ -11,6 +11,18 @@ create table parteVehiculo(
   primary key (id)
 );
 
+insert into parteVehiculo values (1, 'Motor');
+insert into parteVehiculo values (2, 'Llantas');
+insert into parteVehiculo values (3, 'Aire acondicionado');
+insert into parteVehiculo values (4, 'Frenos');
+insert into parteVehiculo values (5, 'Batería');
+insert into parteVehiculo values (6, 'Caja de cambios');
+insert into parteVehiculo values (7, 'Suspensión');
+insert into parteVehiculo values (8, 'Embrague');
+insert into parteVehiculo values (9, 'Sistema eléctrico');
+insert into parteVehiculo values (10, 'Dirección');
+
+
 /* Empleado */
 
 create table empleado(
@@ -20,6 +32,18 @@ create table empleado(
   contrasenia varchar(20) not null,
   primary key (id)
 );
+
+insert into empleado values (1, 'empleado1', 'usuario1', '121');
+insert into empleado values (2, 'empleado2', 'usuario2', '122');
+insert into empleado values (3, 'empleado3', 'usuario3', '123');
+insert into empleado values (4, 'empleado4', 'usuario4', '124');
+insert into empleado values (5, 'empleado5', 'usuario5', '125');
+insert into empleado values (6, 'empleado6', 'usuario6', '126');
+insert into empleado values (7, 'empleado7', 'usuario7', '127');
+insert into empleado values (8, 'empleado8', 'usuario8', '128');
+insert into empleado values (9, 'empleado9', 'usuario9', '129');
+insert into empleado values (10, 'empleado10', 'usuario10', '120');
+insert into empleado (nombre, usuario, contrasenia) values ('empleado11', 'usuario10', '121');
 
 /* Vehiculo */
 
@@ -31,12 +55,18 @@ create table vehiculo (
   PRIMARY KEY (placa)
 );
 
+insert into vehiculo values('ABC111', 'Opel', 'Corsa', '2008');
+insert into vehiculo values('ABC112', 'Renault', 'Sandero', '2020');
+insert into vehiculo values('ABC113', 'Chevrolet', 'Corsa', '2012');
+insert into vehiculo values('ABC114', 'Kia', 'Sportage', '2016');
+insert into vehiculo values('ABC115', 'Mazda', 'Mazda6', '2019');
+
 /* Revisión */
 
 create table revision (
   id int(20) not null AUTO_INCREMENT,
   estado boolean,
-  fecha timestamp,
+  fechaRevision timestamp,
   vehiculoPlaca varchar(10) not null,
   empleadoId int(20) not null,
   primary key (id),
