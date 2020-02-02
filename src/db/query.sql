efc8051b7f0b0d4da9fd28e6704cd7e2f1fe7ada
Daniel <hdanielmm@gmail.com>
2020-02-02 09:50:09 -0500

    Refactor table's attribute

--- src/db/query.sql
+++ src/db/query.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS sonar;
 /* Maestra partes vehiculo*/
 
 create table parteVehiculo(
-  id int(10) not null,
+  id int(20) not null auto_increment,
   nombre varchar(20),
   primary key (id)
 );
@@ -14,7 +14,7 @@ create table parteVehiculo(
 /* Empleado */
 
 create table empleado(
-  id int(10) not null auto_increment,
+  id int(20) not null auto_increment,
   nombre varchar(20) not null,
   usuario varchar(20) not null,
   contrasenia varchar(20) not null,
@@ -23,22 +23,22 @@ create table empleado(
 
 /* Vehiculo */
 
-CREATE TABLE vehiculo (
-  placa VARCHAR(10) NOT NULL, 
-  marca VARCHAR(25) DEFAULT NULL, 
-  linea VARCHAR(25) DEFAULT NULL, 
-  modelo VARCHAR(25) DEFAULT NULL,
+create table vehiculo (
+  placa varchar(10) not null, 
+  marca varchar(25) DEFAULT null, 
+  linea varchar(25) DEFAULT null, 
+  modelo varchar(25) DEFAULT null,
   PRIMARY KEY (placa)
 );
 
 /* Revisión */
 
-CREATE TABLE revision (
-  id INT(10) NOT NULL AUTO_INCREMENT,
+create table revision (
+  id int(20) not null AUTO_INCREMENT,
   estado boolean,
   fecha timestamp,
   vehiculoPlaca varchar(10) not null,
-  empleadoId int(10) not null,
+  empleadoId int(20) not null,
   primary key (id),
   foreign key (vehiculoPlaca) references vehiculo (placa),
   foreign key (empleadoId) references empleado (id)
@@ -47,12 +47,12 @@ CREATE TABLE revision (
 /* Revision parte vehiculo*/
 
 create table revisionParteVehiculo(
-  id int(10) not null,
+  id int(20) not null auto_increment,
   fechaRevison timestamp not null,
   diagnostico varchar(255),
-  revisionId int(10) not null, 
-  parteVehiculoId int(10),
-  tecnicoId int(10) not null,
+  revisionId int(20) not null, 
+  parteVehiculoId int(20) not null,
+  tecnicoId int(20) not null,
   primary key (id),
   foreign key (tecnicoId) references empleado (id),
   foreign key (revisionId) references revision (id),
